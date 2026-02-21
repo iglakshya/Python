55d9978fa62dae52729bdf3b9384df1f75a1c10f
Lakshya Sharma <lakshyasharma1423@gmail.com>
2026-02-21 13:23:03 +0530

    Create 09_Program.py

---
+++ 09_Program.py
@@ -0,0 +1,14 @@
+def two_sum(nums, target):
+    seen = {}
+
+    for i, num in enumerate(nums):
+        need = target - num
+
+        if need in seen:
+            return [seen[need], i]
+
+        seen[num] = i
+nums = [2, 7, 11, 15]
+target = 9
+
+print(two_sum(nums, target))
